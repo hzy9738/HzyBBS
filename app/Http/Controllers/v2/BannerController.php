a0f1955ec99365cbdddf69d08b6d80fc95b5aa04
hzy9738 <hzy9738@126.com>
2018-06-21 17:06:27 +0800

    HzyBBS
    HzyBBS论坛

---
+++ app/Http/Controllers/v2/BannerController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hzy
+ * Date: 2018/6/21
+ * Time: 上午11:37
+ */
+
+namespace App\Http\Controllers\v2;
+
+
+class BannerController
+{
+    /**
+     * 获取指定 $id 的bannner信息
+     * @param $id
+     */
+    public function getBanner($id){
+        echo 2;
+    }
+}
