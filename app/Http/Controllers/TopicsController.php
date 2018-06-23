460e82ca6c6f4a61fc42b8b7306d49bee1d99007
hzy9738 <hzy9738@126.com>
2018-06-23 14:58:19 +0800

    List topics created by user

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,15 +14,21 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-        $topics = Topic::with('user', 'category')->paginate(15);
-		return view('topics.index', compact('topics'));
-	}
-
-    public function show(Topic $topic)
+    public function index(Request $request, Topic $topic)
     {
-        return view('topics.show', compact('topic'));
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        return view('topics.index', compact('topics'));
+    }
+
+    public function show(Category $category, Request $request, Topic $topic)
+    {
+        // 读取分类 ID 关联的话题，并按每 20 条分页
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->paginate(20);
+
+        // 传参变量话题和分类到模板中
+        return view('topics.index', compact('topics', 'category'));
     }
 
 	public function create(Topic $topic)
